bcdc9ba6d11e9fe4a6d3f651508650d1c7e013bc
Jack (Pixegami) <pixegami@gmail.com>
2023-08-18 22:31:33 +1000

    Initial commit

---
+++ test/docker-lambda-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DockerLambdaAws from '../lib/docker-lambda-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/docker-lambda-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DockerLambdaAws.DockerLambdaAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
